d5d8acbb2a67ace0f4fe59c2460f2fa406b5faaa
Bahadir Kurban <ybahadirkurban@gmail.com>
2020-08-01 15:28:26 +0300

    Follow player script, animation updates
    Created FollowPlayer Script for camerra
    Made animations ready to be used in animation controller

---
+++ Sky jumper/Assets/FollowPlayer.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+    [SerializeField] Transform target;
+
+    private Vector3 initialPos;
+    float offsetz;
+    // Start is called before the first frame update
+    void Start()
+    {
+        initialPos=new Vector3(transform.position.x,transform.position.y,transform.position.z);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        offsetz = target.position.z - transform.position.z;
+        transform.position=new Vector3(target.position.z-offsetz, initialPos.y,initialPos.z);
+    }
+}
